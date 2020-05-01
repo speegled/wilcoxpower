e02ef34cf34d3b6af42233e4fb4e89b29f1ee69a
Darrin Speegle <speegled@slu.edu>
2020-05-01 16:08:16 -0500

    find prob and compute effect size. initial commit

---
+++ R/compute_effsize.R
@@ -0,0 +1,16 @@
+#' Calculates the effect size given two prob distributions on finite set
+#'
+#' @param p1 A vector of probs.
+#' @param p2 A second vector of probs.
+#' @return The Vargha and Delaney's A effect size of a population with probability distributions given by p1 and p2.
+#' @examples
+#' compute_effsize(c(.5, .5), c(.4, .6)
+
+compute_effsize <- function(p1, p2) {
+  p1 <- p1/sum(p1)
+  p2 <- p2/sum(p2)
+  m <- outer(p1, p2)
+  s1 <- sum(diag(m)) * 1/2
+  m[upper.tri(m, diag = TRUE)] <- 0
+  return(sum(m) + s1)
+}
---
+++ R/find_probs.R
@@ -0,0 +1,64 @@
+#' Create distributions with desired effect size
+#'
+#' @param effect_size A numeric value between 0 and 1, Vargha and Delaney's A
+#' @param num_levels The number of levels in the responses.
+#' @param p1 Optional nonnegative values for the distribution of population 1. These will be normalized to sum to 1..
+#' @param p2 Optional nonnegative values for the distribution of population 2. Will be normalized to sum to 1.
+#' @return Returns population probabilities associated with the levels to achieve desired effect size. These are not unique.
+#' @examples
+#' find_probs(.6, 3)
+#' find_probs(.6, 3, rep(1, 3))
+
+find_probs <- function(effect_size, num_levels, p1, p2) {
+  if(missing(p2) && !missing(p1)) {
+    p2 <- runif(1:num_levels, 0, 1)
+    dat <- list(num_levels = num_levels,
+                      goal = effect_size,
+                      p1 = p1)
+    to_min <- function(par, dat) {
+      (compute_effsize(dat$p1, par[1:dat$num_levels]) - dat$goal)^2
+    }
+    probs <- constrOptim(p2,
+                         f = to_min,
+                         ui = diag(num_levels),
+                         ci = 0,
+                         method = "Nelder-Mead",
+                         dat = dat)
+    if(probs$value < .0001) {
+      p1 <- p1/sum(p1)
+      p2 <- probs$par/sum(probs$par)
+      return(list(p1 = p1,
+                  p2 = p2,
+                  effect_size = compute_effsize(p1, probs$par),
+                  message = probs$message))
+    } else {
+      warning("Effect size not possible with provided p1")
+    }
+   }
+
+  if(missing(p1)) {
+    p1 <- runif(1:num_levels, 0, 1)
+  }
+  if(missing(p2)) {
+    p2 <- runif(1:num_levels, 0, 1)
+  }
+
+  to_min <- function(par, dat) {
+    (compute_effsize(par[1:dat$num_levels], par[(dat$num_levels+1):(2 * dat$num_levels)]) - dat$goal)^2
+  }
+
+  dat <- data.frame(num_levels = num_levels,
+                    goal = effect_size)
+  probs <- constrOptim(c(p1, p2),
+                       f = to_min,
+                       ui = diag(2 * num_levels),
+                       ci = 0,
+                       method = "Nelder-Mead",
+                       dat = dat)
+  p1 <- probs$par[1:num_levels]
+  p2 <-  probs$par[(num_levels + 1):(2 * num_levels)]
+  return(list(p1 = p1/sum(p1),
+              p2 = p2/sum(p2),
+              effect_size = compute_effsize(p1, p2),
+              message = probs$message))
+}
