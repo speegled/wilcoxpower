2e45670a193995d3ce0513666d8255e1f8411c2b
Darrin Speegle <speegled@slu.edu>
2020-05-04 09:49:04 -0500

    more package stuff

--- R/compute_effsize.R
+++ R/compute_effsize.R
@@ -4,7 +4,7 @@
 #' @param p2 A second vector of probs.
 #' @return The Vargha and Delaney's A effect size of a population with probability distributions given by p1 and p2.
 #' @examples
-#' compute_effsize(c(.5, .5), c(.4, .6)
+#' compute_effsize(c(.5, .5), c(.4, .6))
 
 compute_effsize <- function(p1, p2) {
   p1 <- p1/sum(p1)
